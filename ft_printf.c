9cab332e8ed8a4b2c95d1f894502c6732978166a
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2024-04-18 20:22:13 +0200

    Init commit

---
+++ ft_printf.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: labdello <labdello@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 19:57:23 by labdello          #+#    #+#             */
+/*   Updated: 2024/04/18 20:04:20 by labdello         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+
+int	ft_printf(const char *format, ...)
+{
+	(void)format;
+	return (0);
+}
+
+#include <stdio.h>
+int main()
+{
+	printf("%s\n", "test42");
+	ft_printf("%s\n", "test42");
+	return (0);
+}
