9ad50ef972cc8ae24d4d228ecc4c7781e4488080
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2024-04-22 12:05:27 +0200

    progress-1

--- ft_printf.c
+++ ft_printf.c
@@ -6,16 +6,29 @@
 /*   By: labdello <labdello@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 19:57:23 by labdello          #+#    #+#             */
-/*   Updated: 2024/04/18 22:39:48 by labdello         ###   ########.fr       */
+/*   Updated: 2024/04/21 17:42:49 by labdello         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-// #include "libft.h"
+
+size_t	get_arg_count(const char *format)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	return (count);
+}
 
 int	ft_printf(const char *format, ...)
 {
 	(void)format;
+	va_list	args;
+	size_t	arg_count;
+
+	arg_count = get_arg_count(format);
 	return (0);
 }
 
@@ -23,7 +36,7 @@ int	ft_printf(const char *format, ...)
 int main()
 {
 	ft_putchar('c');
-	printf("%s\n", "test42");
+	printf("%s\n", "test42", "test");
 	ft_printf("%s\n", "test42");
 	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,13 +6,16 @@
 /*   By: labdello <labdello@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 22:16:35 by labdello          #+#    #+#             */
-/*   Updated: 2024/04/19 11:52:33 by labdello         ###   ########.fr       */
+/*   Updated: 2024/04/21 17:40:29 by labdello         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
 # include <stdarg.h>
 
 void	ft_putchar(char c);
